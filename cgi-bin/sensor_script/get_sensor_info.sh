255f057566312886dd5aa49474e56c5a3e9d7450
kindmong <kindmong@ubuntu.(none)>
2015-12-24 16:58:44 +0900

    di do 수정

--- cgi-bin/sensor_script/get_sensor_info.sh
+++ cgi-bin/sensor_script/get_sensor_info.sh
@@ -5,3 +5,5 @@ ip=`/home/kindmong/work/ftm_manager/cgi-bin/sensor_script/search_mac.sh $1`
 
 /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_humidity_info.sh $ip | awk -f /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_humidity_info.awk
 /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_temperature_info.sh $ip | awk -f /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_temperature_info.awk
+/home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_di_info.sh $ip | awk -f /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_di_info.awk
+/home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_do_info.sh $ip | awk -f /home/kindmong/work/ftm_manager/cgi-bin/sensor_script/get_do_info.awk
